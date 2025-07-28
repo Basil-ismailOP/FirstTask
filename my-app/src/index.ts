815e50d3a959b61c07b4c4d0042c1b9cd127fd51
Basel Ismail <baselismail@basil-ismail-linux.scoiumtech.local>
2025-07-28 17:31:13 +0300

    To be continued tomorrow, Currently working on PostgreSQL

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,10 +1,15 @@
+import "dotenv/config";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { postsRoutes } from "./routes/postsRoutes";
 import { userRoutes } from "./routes/userRoutes";
+import { drizzle } from "drizzle-orm/singlestore/driver";
+import { configDotenv } from "dotenv";
 
+const db = drizzle(process.env.POSTGRESS_CONNECTION as string);
 const app = new Hono();
 
+const result = await db.execute("select 1");
 app.use("*", logger());
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -12,4 +17,5 @@ app.get("/", (c) => {
 
 app.route("/api/posts", postsRoutes);
 app.route("/api/user", userRoutes);
+
 export default app;
